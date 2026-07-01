774704620587b9dc1890d92a5d2313c5e9ae916a
EdgeOne Pages <noreply@edgeone.com>
2026-07-01 10:30:16 +0800

    feat: init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { PageLayout } from '@/components/layout'
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
+
+export default function Home() {
+  return (
+    <PageLayout>
+      <Hero />
+      <Features />
+    </PageLayout>
+  )
+}
---
+++ src/components/layout/index.ts
@@ -0,0 +1,3 @@
+export { default as PageLayout } from './PageLayout'
+export { default as DemoLayout } from './DemoLayout'
+export { default as DataDisplay } from './DataDisplay' 
